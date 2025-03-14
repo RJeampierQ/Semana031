b1d613cc3a9a74dc48a4059e6c18cbfdf7f813c8
RJeampierQ <jaze2912@gmail.com>
2025-03-13 22:46:54 -0500

    HI

--- Mostrar.php
+++ Mostrar.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Empleados</title>
+    <title>Lista de Egregmpleados</title>
 </head>
 <body>
-    <h1>Lista de Empleados</h1>
+    <h1>Lista eggererde Empleados</h1>
     <?php
     echo "<table border='1'>
             <tr>
--- index.php
+++ index.php
@@ -11,7 +11,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplicación web de empleados</title>
+    <title>g web de emgwgwrgrggpleados</title>
 </head>
 <body>
     <h1>Gestión de Empleados</h1>
