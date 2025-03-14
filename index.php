3a7ff6c73c6d1aeda26070b96dcb7f4e48319fc9
RJeampierQ <jaze2912@gmail.com>
2025-03-14 12:29:41 -0500

    MODIFICACION EN EL INDEX

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>g web de emgwgwrgrggpleados</title>
+    <title>HOLA MUNDO web de emgwgwrgrggpleados</title>
 </head>
 <body>
     <h1>Gestión de Empleados</h1>
