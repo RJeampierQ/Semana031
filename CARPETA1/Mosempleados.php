37da244d195b4a118440a44ee008830d39e84948
RJeampierQ <jaze2912@gmail.com>
2025-03-29 12:24:53 -0500

    modificacion en mosempleados

--- CARPETA1/Mosempleados.php
+++ CARPETA1/Mosempleados.php
@@ -9,7 +9,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplicfwefwefwefwefweación web</title>
+    <title>Aplicacion 2</title>
 </head>
 <body>
     <h4>Lista de Empleados</h4>
